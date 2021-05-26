511c76be9268a58fdf5f97371b7202c27b297e3b
frozflame <lendoli@163.com>
2021-05-26 21:15:38 +0800

    python3 only

--- setup.py
+++ setup.py
@@ -51,12 +51,7 @@ config = {
     'install_requires': read("requirements.txt"),
     'classifiers': [
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.0',
-        'Programming Language :: Python :: 3.1',
-        'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
