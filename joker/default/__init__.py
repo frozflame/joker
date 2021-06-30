1a9f80eaa8ef7465f24df4921e54d6bf7d7204ed
frozflame <lendoli@163.com>
2021-06-30 22:27:28 +0800

    ver 0.2.2 -- correct typo: enviorn

--- joker/default/__init__.py
+++ joker/default/__init__.py
@@ -5,8 +5,8 @@ from __future__ import unicode_literals
 
 from volkanic.utils import under_home_dir, under_package_dir
 
-from joker.enviorn import under_joker_dir, make_joker_dir, __version__
-from joker.enviorn.utils import load_modules_under_package
+from joker.environ import under_joker_dir, make_joker_dir, __version__
+from joker.environ.utils import load_modules_under_package
 
 __all__ = [
     'under_home_dir',
