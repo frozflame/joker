d80ee1c095be8a16bd721019026714f6d7f4a66a
frozflame <frozflame@outlook.com>
2021-09-19 22:54:57 +0800

    add test_imports.py

---
+++ tests/test_imports.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+
+import importlib
+
+from volkanic.introspect import find_all_plain_modules
+
+from joker.environ.provincial import JokerInterface
+
+ji = JokerInterface()
+
+
+def test_module_imports():
+    for dotpath in find_all_plain_modules(ji.under_project_dir()):
+        if dotpath.startswith('joker.'):
+            print(dotpath)
+            importlib.import_module(dotpath)
+
+
+if __name__ == '__main__':
+    test_module_imports()
