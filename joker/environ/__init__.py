5522bc0ebaa82ed94d37314f255a455266ecd0ed
frozflame <frozflame@outlook.com>
2021-12-02 21:39:28 +0800

    merge joker.interfaces into this project

--- joker/environ/__init__.py
+++ joker/environ/__init__.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 # coding: utf-8
-__version__ = '0.2.5!'
 
 from joker.environ.generic import GlobalInterface
+from joker.interfaces import __version__
+
+if __name__ == '__main__':
+    print(__version__)
